257acc0356e6e1b8caf0c3444513318cab832c8f
hanneslindberg <hannes.lindberg@elev.ga.ntig.se>
2024-04-11 15:55:28 +0200

    11/04

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import pymunk
+import pymunk.pygame_util
 import time
 import random
 import math
@@ -7,22 +9,47 @@ pygame.init()
 
 WIDTH, HEIGHT = 1000, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
 pygame.display.set_caption("Golfspel")
 
 BG = pygame.image.load("bg.png")
 
-player = [WIDTH/2, HEIGHT/2]
+player = [100, 300]
 max_speed = 110
-player_radius = 10
- 
+player_radius = 15
+
+space = pymunk.Space()
+static_body = space.static_body
+draw_options = pymunk.pygame_util.DrawOptions(WIN)
+
+def create_ball(radius, pos):
+    body = pymunk.Body()
+    body.position = pos
+    shape = pymunk.Circle(body, radius)
+    shape.mass = 5
+    pivot = pymunk.PivotJoint(static_body, body, (0, 0), (0, 0))
+    pivot.max_bias = 0
+    pivot.max_force = 1000
+
+
+    space.add(body, shape, pivot)
+    return shape
+
+ball = create_ball(player_radius, player)
+
+clock = pygame.time.Clock()
+FPS = 60
+
 left_click = False
 
 def main():
     run = True
 
     while run:
+        clock.tick(FPS)
+        space.step(1 / FPS)
+
         left_click = False
+        
 
         mx, my = pygame.mouse.get_pos()
 
@@ -38,35 +65,54 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    left_click = True
+                ball.body.apply_impulse_at_local_point((1000, 0), (0, 0)) 
 
         WIN.blit(BG, (0, 0))
 
         
-        if left_click == True:
-            if length != 0:
-                direction_vector[0] /= length
-                direction_vector[1] /= length
+        # if left_click == True:
+        #     if length != 0:
+        #         direction_vector[0] /= length
+        #         direction_vector[1] /= length
             
-            speed = max_speed * (length / 100)
+        #     speed = max_speed * (length / 100)
 
-            new_ball_pos = [player[0] + direction_vector[0] * speed, player[1] + direction_vector[1] * speed]
+        #     new_ball_pos = [player[0] + direction_vector[0] * speed, player[1] + direction_vector[1] * speed]
+
+            # i = 0
+            # while i <= 100:
+            #     currentPlayerValue = player
+            #     print("Jag är här")
+            #     if player[0] <= new_ball_pos[0]:
+            #         player[0] = 0.1*currentPlayerValue[0]
+            #         # pygame.draw.circle(WIN, "white", (player), player_radius)
+            #     elif player[0] >= new_ball_pos[0]:
+            #         player[0] = 1.1*currentPlayerValue[0]
+            #         # pygame.draw.circle(WIN, "white", (player), player_radius)
+            #     if player[1] <= new_ball_pos[1]:q
+            #         player[1] = 0.1*currentPlayerValue[1]
+            #         # pygame.draw.circle(WIN, "white", (player), player_radius)
+            #     elif player[1] >= new_ball_pos[1]:
+            #         player[1] = 1.1*currentPlayerValue[1]    
+                    # pygame.draw.circle(WIN, "white", (player), player_radius)
+                
+                # pygame.draw.circle(WIN, "white", (player), player_radius)
+
+
+
+
 
             player[0] = new_ball_pos[0]
             player[1] = new_ball_pos[1]
 
         pygame.draw.circle(WIN, "black", [800, 100], player_radius)
-        pygame.draw.line(WIN, "black", (player), (end_point), 3)
-        pygame.draw.circle(WIN, "white", (player), player_radius)
+        #pygame.draw.line(WIN, "black", (player), (end_point), 3)
+        #pygame.draw.circle(WIN, "white", (player), player_radius)
         
+        space.debug_draw(draw_options)
         pygame.display.flip()
             
     pygame.quit()
 
 if __name__ == "__main__":
     main()
-
-
-
-    #Crashar för att man inte kan dividera med "0"
