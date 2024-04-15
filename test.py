38a2667a4ea1c7129827ec09874ab8bb92d69a7e
hanneslindberg <hannes.lindberg@elev.ga.ntig.se>
2024-04-15 18:02:30 +0200

    15/04 v.2

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
-# https://www.youtube.com/watch?v=txcOqDhrwBo&t=1962s
-
-# ha olika typer av väggar som stutsväggar och klibbiga väggar
-
-# Fråga Eirik hur man gör de groparna och kullarna
-
 import pygame
-import pymunk
-import pymunk.pygame_util
-import math
 
-# Initiate pygaem
+# Initiate pygame
 pygame.init()
-BG = pygame.image.load("img/bg.png")
+
 # Create window
+BG = pygame.image.load("img/bg.png")
 WIDTH, HEIGHT = 1000, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Golfspel")
